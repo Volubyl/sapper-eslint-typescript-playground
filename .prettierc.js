274521d83e0c425a1a7b0825fcee84190613a005
BertrandD <volubyl@pm.me>
2021-03-13 13:09:03 +0100

    Configure prettier and eslint

---
+++ .prettierc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  singleQuote: true,
+  plugins: ["prettier-plugin-svelte"],
+  semi: false,
+  svelteSortOrder: "options-styles-scripts-markup",
+  svelteStrictMode: false,
+  svelteBracketNewLine: true,
+  svelteIndentScriptAndStyle: true,
+};
